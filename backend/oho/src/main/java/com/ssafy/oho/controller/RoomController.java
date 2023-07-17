143266024edc6cce08be7ac5b79fed259d9e078e
임혜지 <hyeji4775@naver.com>
2023-07-17 15:21:01 +0900

    Feat: #82[혜지] Entity 등의 클래스 생성

--- backend/oho/src/main/java/com/ssafy/oho/controller/RoomController.java
+++ backend/oho/src/main/java/com/ssafy/oho/controller/RoomController.java
@@ -1,4 +1,20 @@
 package com.ssafy.oho.controller;
 
+import com.ssafy.oho.model.dto.request.MemberRequestDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class RoomController {
+
+    @PostMapping(value="/enter")
+    private ResponseEntity<?> setRoom(@RequestBody MemberRequestDto memberRequestDto){
+
+        return ResponseEntity.ok("ok");
+    }
+
 }
