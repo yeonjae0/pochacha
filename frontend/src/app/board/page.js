ffbcfa8f2bf7b0c33742545a0d45a8ff2fcdccf7
배희진 <dilook0424@naver.com>
2023-07-25 01:25:55 +0900

    Feat: #157[희진] 말 이동 테스트(useEffect 이용)

--- frontend/src/app/board/page.js
+++ frontend/src/app/board/page.js
@@ -1,6 +1,5 @@
 "use client"
-import MapComponent from '@/routes/MapComponent';
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './../map.css';
 
 export default function Board() {
@@ -8,13 +7,19 @@ export default function Board() {
   let [dice, setDice] = useState(0); // 주사위
   let [pin, setPin] = useState(0); // 현재 위치
   let [lab, setLab] = useState(0); // 바퀴 수
-  let road = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      const element = document.getElementById('block'+pin);
+      element.innerHTML += '<img src="./character.png" />';
+    }, 1000);
+  }, [pin])
 
   return (
     <div>
       <h1>보드게임 화면</h1>
 
-      <button onClick={()=>{
+      <button value="innerHTML" onClick={()=>{
         let num = 6;
         let dice = parseInt(Math.random() * num + 1); // 랜덤으로 주사위 숫자 추출
         setDice(dice); // 말 수 변경
@@ -23,10 +28,23 @@ export default function Board() {
         {
           pin+dice <= 24
           ? (
-            setPin(pin+dice)
+            setPin(pin+dice+1)
           )
-          : setPin(pin+dice-24);
+          : setPin(pin+dice-23)
         }
+        
+        // setTimeout(()=>{
+        //   const element = document.getElementById('block'+pin);
+        //   element.innerHTML += '<img src="./character.png" />';
+        // }, 1000);
+
+        // 반복문을 사용하지 않는다면, DOM에 접근할 수 있는 방법이 있어야 함, 방법을 찾아야 함
+        // curPin ~ pin 으로 이동하는 경로를 하나씩 반복문으로 보여주기
+        // const element = document.getElementById('block'+pin);
+        // element.innerHTML += '<img src="./character.png" />';
+    
+        // setTimeout(()=>{
+        // element.innerHTML = '' }, 1000);
 
         // 한 바퀴 돌 때마다 lab state 변경
         {
@@ -51,47 +69,47 @@ export default function Board() {
         { lab }바퀴 돌았습니다.
       </div>
 
-      <div class='board' >
+      <div className='board'>
         <div id='board-r1' style={{ display: 'flex' }}>
-          <div id='block1'></div>
-          <div id='block2'></div>
-          <div id='block3'></div>
-          <div id='block4'></div>
-          <div id='block5'></div>
-          <div id='block6'></div>
-          <div id='block7'></div>
+          <div id='block19' value="innerHTML">{}</div>
+          <div id='block18' value="innerHTML"></div>
+          <div id='block17' value="innerHTML"></div>
+          <div id='block16' value="innerHTML"></div>
+          <div id='block15' value="innerHTML"></div>
+          <div id='block14' value="innerHTML"></div>
+          <div id='block13' value="innerHTML"></div>
         </div>
         <div id='board-r2' style={{ display: 'flex' }}>
-          <div id='block8'></div>
-          <div id='block9'></div>
+          <div id='block20' value="innerHTML"></div>
+          <div id='block12' value="innerHTML"></div>
         </div>
         <div id='board-r3' style={{ display: 'flex' }}>
-          <div id='block10'></div>
-          <div id='block11'></div>
+          <div id='block21' value="innerHTML"></div>
+          <div id='block11' value="innerHTML"></div>
         </div>
         <div id='board-r4' style={{ display: 'flex' }}>
-          <div id='block12'></div>
-          <div id='block13'></div>
+          <div id='block22' value="innerHTML"></div>
+          <div id='block10' value="innerHTML"></div>
         </div>
         <div id='board-r5' style={{ display: 'flex' }}>
-          <div id='block14'></div>
-          <div id='block15'></div>
+          <div id='block23' value="innerHTML"></div>
+          <div id='block9' value="innerHTML"></div>
         </div>
         <div id='board-r6' style={{ display: 'flex' }}>
-          <div id='block16'></div>
-          <div id='block17'></div>
+          <div id='block24' value="innerHTML"></div>
+          <div id='block8' value="innerHTML"></div>
         </div>
         <div id='board-r7' style={{ display: 'flex' }}>
-          <div id='block1'></div>
-          <div id='block2'></div>
-          <div id='block3'></div>
-          <div id='block4'></div>
-          <div id='block5'></div>
-          <div id='block6'></div>
-          <div id='block7'></div>
+          <div id='block1' value="innerHTML"></div>
+          <div id='block2' value="innerHTML"></div>
+          <div id='block3' value="innerHTML"></div>
+          <div id='block4' value="innerHTML"></div>
+          <div id='block5' value="innerHTML"></div>
+          <div id='block6' value="innerHTML"></div>
+          <div id='block7' value="innerHTML"></div>
         </div>
 
-    </div>
+      </div>
     </div>
   )
 }
