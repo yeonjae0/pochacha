141b613d355f23dd4346fe42392a6cd9ce78d7d4
Ubuntu <ubuntu@ip-172-26-3-152.ap-northeast-2.compute.internal>
2023-07-25 15:23:02 +0000

    Feat: #227[태훈] nginx 서버에 ssl 적용

--- frontend/src/pages/test/GetPage.js
+++ frontend/src/pages/test/GetPage.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
-
+import https from "https";
 
 function GetPage(){
     
     const [testId, setTestId] = useState(0);
     const [testInfo, setTestInfo] = useState("No Info");
 
-    const BASEURL = "http://52.78.56.80:8080"
+    const BASEURL = "https://43.201.150.143:8443"
+
+    const agent = new https.Agent({  
+        rejectUnauthorized: false
+    });
 
     useEffect(()=>{
-        axios.get(BASEURL + "/api/get")
+        axios.get(BASEURL + "/api/get", { httpsAgent: agent})
         .then((res)=>{
             console.log(res)
             setTestId(res.data[0].id)
--- frontend/src/pages/test/PostPage.js
+++ frontend/src/pages/test/PostPage.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
-
+import https from "https";
 
 function PostPage(){
     
     // const [testId, setTestId] = useState(0);
     const [info, setInfo] = useState("No Info");
-    const BASEURL = "http://52.78.56.80:8080"
+    const BASEURL = "https://43.201.150.143:8443"
+
+    const agent = new https.Agent({  
+        rejectUnauthorized: false
+    });
 
     const pressButton = (param) => {
         axios.post(BASEURL + "/api/post", JSON.stringify({info: param}), {
-            headers: { "Content-Type": `application/json`}
+		headers: { "Content-Type": `application/json`, "httpsAgent": agent}
             }).then((res)=>console.log(res))
     }
     
