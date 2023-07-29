c2ebea544532cc51cd7e3f20e8007af52f7bd6bf
김태훈 <zaqxsw1397@naver.com>
2023-07-29 09:58:35 +0000

    Feat: #270[태훈] 프론트엔드 배포 및 개발 환경 세팅 완료

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  "exportTrailingSlash": true,
+  "output": "export"
+}
 
 module.exports = nextConfig
--- frontend/src/app/first/page.js
+++ frontend/src/app/first/page.js
@@ -72,7 +72,7 @@ export default function Room() {
   /* 유영 : axios를 통한 닉네임 생성 및 방 생성 시작 */
   const start = () => {
     axios({
-      url : {REACT_APP_HOST},//"http://localhost:80/enter",
+	    url : process.env.NEXT_PUBLIC_HOST + "/enter",//"http://localhost:80/enter",
       header : {
         "Accept" : "application/json",
         "Content-type" : "aplic ation/json;charset=UTF-8"
@@ -97,7 +97,7 @@ export default function Room() {
     }, 2000);
 
     /* 유영 : 소켓 간단 연결 작업 시작 */
-    const socket = new SockJS({REACT_APP_HOST} + "/ws");
+    const socket = new SockJS(process.env.NEXT_PUBLIC_HOST + "/wss");
     // const socket = new SockJS("http://localhost:80/ws");
     const stompClient = Stomp.over(socket);
 
