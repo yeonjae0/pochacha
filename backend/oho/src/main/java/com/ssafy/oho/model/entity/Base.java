ea8a6e4127a25e6d4c545a7d9ed704de3d94604e
김태훈 <zaqxsw1397@naver.com>
2023-08-04 17:24:03 +0900

    Fix: #400[태훈] @CreatedDate와 @LastModifiedDate 어노테이션 적용을 통한 Base 엔티티 수정

--- backend/oho/src/main/java/com/ssafy/oho/model/entity/Base.java
+++ backend/oho/src/main/java/com/ssafy/oho/model/entity/Base.java
@@ -3,27 +3,20 @@ package com.ssafy.oho.model.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 /* 생성 날짜 및 수정 날짜 일괄 관리 */
 public class Base {
 
-    @Column(name="created_at",updatable = false)
+    @CreatedDate
     private LocalDateTime createdAt;
-    @Column(name="updated_at")
+    @LastModifiedDate
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    public void prePersist(){
-        LocalDateTime now = LocalDateTime.now();
-        createdAt = now;
-        updatedAt = now;
-    }
-    @PreUpdate
-    public void preUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
 }
