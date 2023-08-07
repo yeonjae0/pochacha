de84e402104d16f4aecf7ecf742cace43192b710
heejinssss <heejin.dev33@gmail.com>
2023-08-07 17:48:22 +0900

    Feat: #547[희진] 두더지 게임 MVP

--- frontend/pages/game/mini/mole/index.js
+++ frontend/pages/game/mini/mole/index.js
@@ -1,52 +1,60 @@
 import { useState, useEffect } from 'react';
 
-export default function MoleGame () {
+const getRandomGridPosition = () => ({
+  row: Math.floor(Math.random() * 3), // 0, 1, or 2
+  col: Math.floor(Math.random() * 3), // 0, 1, or 2
+});
+
+export default function Home() {
   const [score, setScore] = useState(0);
-  const [moleUp, setmoleUp] = useState(false);
+  const [molePosition, setMolePosition] = useState(getRandomGridPosition());
 
-  // 두더지가 나타나는 빈도와 게임 진행 간격을 설정합니다.
-  const moleInterval = 2000; // 깜빡이 빈도, 2초
-  const gameTime = 20000; // 게임 진행 시간, 20초
-
-  // 게임 시작 후 일정 시간마다 두더지를 보이게 하거나 숨기는 함수
-  useEffect(() => {
-    const moleTimer = setInterval(() => {
-      setmoleUp(true);
-      setTimeout(() => {
-        setmoleUp(false);
-      }, moleInterval * 0.5) // 두더지가 보이는 시간 (1초)
-    }, moleInterval)
-
-    setTimeout(() => {
-      clearInterval(moleTimer)
-      alert('Game Over! Your score: ' + score);
-    }, gameTime)
-
-    return () => {
-      clearInterval(moleTimer)
-    }
-  }, [score])
-
-  // 두더지를 클릭했을 때 점수를 올리는 함수
   const handleMoleClick = () => {
-    if (moleUp) {
-      setScore((prevScore) => prevScore + 1)
-    }
-  }
+    setScore(score + 1);
+    setMolePosition(getRandomGridPosition());
+  };
+
+  useEffect(() => {
+    const moleTimer = setTimeout(() => {
+      setMolePosition(getRandomGridPosition());
+    }, 1000);
+
+    return () => clearTimeout(moleTimer);
+  }, [molePosition]);
+
+  const moleImagePosition = {
+    top: `${molePosition.row * 33.33}%`,
+    left: `${molePosition.col * 33.33}%`,
+  };
 
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       <h1>두더지 게임</h1>
-      <p>Score: {score}</p>
+      <h2>점수: {score}</h2>
       <div
         style={{
-          width: '100px',
-          height: '100px',
-          backgroundColor: moleUp ? 'brown' : 'transparent',
-          cursor: moleUp ? 'pointer' : 'default',
+          position: 'relative',
+          width: '300px',
+          height: '300px',
+          border: '2px solid #000',
+          margin: '0 auto',
+          overflow: 'hidden',
         }}
-        onClick={handleMoleClick}
-      />
+      >
+        <img
+          src="/mole1.png"
+          alt="두더지"
+          onClick={handleMoleClick}
+          style={{
+            position: 'absolute',
+            top: moleImagePosition.top,
+            left: moleImagePosition.left,
+            width: '100px',
+            height: '100px',
+            cursor: 'pointer',
+          }}
+        />
+      </div>
     </div>
-  )
+  );
 }
