7bd09a3ab5a9896855e06a72373e560ed045a185
heejinssss <heejin.dev33@gmail.com>
2023-08-07 16:26:01 +0900

    Deploy: #512[희진] 빌드 테스트용

---
+++ frontend/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <p>메인 배경색 수정 가능</p>
+      <Link href={'/enter'}><h3>🙌대기실 (START 클릭 시 Go To Room)</h3></Link>
+      <Link href={'/game'}><h3>🤩메인 게임</h3></Link>
+      <Link href={'/game/mini/liar'}><h3>😋라이어 게임</h3></Link>
+      <Link href={'/game/mini/mole'}><h3>🦔두더지 게임</h3></Link>
+      <Link href={'/game/mini/spell'}><h3>🦄훈민정음 게임</h3></Link>
+    </div>
+  )
+}
