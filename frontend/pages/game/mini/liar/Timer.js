848df3332c0441bc05bc7efaed08be0d9fb0d88a
임혜지 <hyeji4775@naver.com>
2023-08-08 11:32:18 +0900

    Feat: #589[혜지] 타이머 관련 테스트 코드 작성

---
+++ frontend/pages/game/mini/liar/Timer.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+
+export default function Timer(){
+    const [timeLeft,setTimeLeft]=useState(60);
+    const [timerRunning,setTimerRunning]=useState(false);
+
+    const socket=new SockJS('http://localhost:80/socket');
+    //websocket server url 수정 필요
+
+    useEffect(()=>{
+        const stompClient=Stomp.over(socket);
+
+        stompClient.connect({},frame=>{
+            stompClient.subscribe('/topic/timer',message=>{
+                const parsedMessage=JSON.parse(message.body);
+                setTimeLeft(parsedMessage.timeLeft);
+            });
+        });
+
+        return ()=>{
+            stompClient.disconnect();
+        }
+    },[]);
+
+    const startTimer=()=>{
+        setTimerRunning(true);
+    };
+
+    const stopTimer=()=>{
+        setTimerRunning(false);
+    };
+
+    return(
+        <div>
+            <h1>타이머 테스트</h1>
+            <p>Time Left: {timeLeft} seconds</p>
+            <button onClick={startTimer}>Start Timer</button>
+            <button onClick={stopTimer}>Stop Timer</button>
+        </div>
+    );
+}
