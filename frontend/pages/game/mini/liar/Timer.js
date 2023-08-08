b8171544e1c2fbf222b3e02b82f67e75fee46daa
임혜지 <hyeji4775@naver.com>
2023-08-08 14:24:48 +0900

    Feat: #589[혜지] 투표페이지(라디오버튼 보완 필요)

--- frontend/pages/game/mini/liar/Timer.js
+++ frontend/pages/game/mini/liar/Timer.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 
+/*
+    CONFIRM :: 두더지게임 타이머를 적용할지 고민
+*/
 export default function Timer(){
     const [timeLeft,setTimeLeft]=useState(60);
     const [timerRunning,setTimerRunning]=useState(false);
 
-    const socket=new SockJS('http://localhost:80/socket');
+    const socket=new SockJS('http://localhost:80/ws');
     //websocket server url 수정 필요
 
     useEffect(()=>{
--- frontend/pages/game/mini/liar/VoteLiarComponent.js
+++ frontend/pages/game/mini/liar/VoteLiarComponent.js
@@ -1,6 +1,32 @@
-export default function VoteLiarComponent(){
+import React from "react";
+
+
+export default function VoteLiarComponent({nicknames}){
+
+    /* 임시의 플레이어 리스트 */
+    console.log(nicknames);
 
     return (
-        <div>투표 진행 컴포넌트</div>
-    )
+        <div>
+            <h1>누가 거짓말을 하고 있을까?</h1>
+            <fieldset>
+       { nicknames.map((nickname, i) => {
+            return (
+                <label>
+                    <input
+                        type="radio"
+                        value={nickname}
+                        name={nickname}
+                        // defaultChecked={defaultChecked}
+                        // disabled={disabled}
+                    />
+                    {nicknames[i]}
+                    <br/>
+                </label>
+            )
+          })
+        }
+        </fieldset>
+        </div>
+        )
 }
