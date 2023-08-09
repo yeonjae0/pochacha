c2b8b5f4fc5f8c317378b5208f9f7ec152b19f10
배희진 <dilook0424@naver.com>
2023-08-09 11:28:59 +0900

    Fix: #452[희진] 주사위 UI 수정

--- frontend/pages/game/[id]/DiceBox.js
+++ frontend/pages/game/[id]/DiceBox.js
@@ -1,46 +1,47 @@
 import React, { useState, useEffect, useRef } from 'react'
-import Dice from './Dice'
-import styles from '@/styles/DiceBox.module.css'
+import Dice from './Dice.js'
+import styles from '@/styles/Home.module.css';
 
 export default function DiceBox(props) {
+
+  console.log('DiceBox props', props)
 
   const [diceWidth, setDiceWidth] = useState(0) // 주사위 가로 길이
   const [dice, setDice] = useState(1) // 주사위 눈 값
 
-  const diceRef = useRef(null)
-  
+  const diceRef = useRef(null);
+
   useEffect(() => {
 
     /* 브라우저 width 값 변화할 때 */
     const updateDiceWidth = () => {
       if (diceRef.current) {
-        setDiceWidth(diceRef.current.clientWidth)
+        setDiceWidth(diceRef.current.clientWidth);
         console.log(diceWidth)
       }
     }
-    
-    window.addEventListener('resize', updateDiceWidth)
+
+    window.addEventListener('resize', updateDiceWidth);
     updateDiceWidth()
     return () => {
-      window.removeEventListener('resize', updateDiceWidth)
-    }
-    
+      window.removeEventListener('resize', updateDiceWidth);
+    };
+
   }, [diceWidth])
-  
+
   const rolling = (props) => {
-    let ranNum = Math.floor(Math.random() * 6) + 1
+    // let ranNum = Math.floor(Math.random() * 6) + 1
     setDice(props.dice)
   }
 
-    return (
+  return (
     <section className={styles.diceBox} ref={diceRef}>
-      {/* <div className={styles.dice_wrap}> */}
-      <div style={{ width: '100%', height: 'auto', marginBottom: '20px' }}>
+      <div className={styles.dice_wrap}>
         <Dice face={props.dice} diceWidth={diceWidth} />
       </div>
-      <button id="btnRolling" onClick={rolling}>
+      {/* <button id="btnRolling" onClick={rolling}>
         주사위 굴리기
-      </button>
+      </button> */}
     </section>
-    )
+  )
 }
