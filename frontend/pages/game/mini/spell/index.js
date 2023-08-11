96370b1417cd04946a81356b001d4ff357b13ed5
김연재 <yjkim9195@gmail.com>
2023-08-11 11:26:18 +0900

    Fix: #501[연재] 초성게임 오류 -ing

--- frontend/pages/game/mini/spell/index.js
+++ frontend/pages/game/mini/spell/index.js
@@ -5,6 +5,7 @@ import styles from "@/styles/SpellGame.module.css";
 import axios from 'axios'
 import SockJS from 'sockjs-client'
 import { Stomp } from '@stomp/stompjs'
+import { saveWord } from '@/store/reducers/spell'
 
 const getConsonant = () => {
 
@@ -13,8 +14,9 @@ const getConsonant = () => {
   const [randomConsonant, setRandomConsonant] = useState("");
   const [inputWords, setInputWords] = useState([]);  // 입력한 단어들 저장
   const [inputValue, setInputValue] = useState("");  // 유저 입력값 저장
-  const roomId = useSelector(state => state.room.currentRoomId);
   const [client, setClient] = useState({});
+  const roomId = useSelector(state => state.room.currentRoomId);
+  const currentword = useSelector(state => state.spell.currentWord);
 
   const router = useRouter()
 
@@ -30,11 +32,13 @@ const getConsonant = () => {
   };
 
   const handleSubmit = () => {
-    if (inputValue.trim() !== "") {
-      if (client.current) {
+    if (inputValue.trim() && client.current) {
         let sendData = {
           "word": inputValue,
         }
+        // setInputValue(sendData)
+        dispatch(saveWord({input: inputValue}))
+        console.log('currentword111', currentword)
         console.log('inputValue', inputValue)
         console.log('sendData', sendData)
         // console.log('여기까지..?', data.correct) 
@@ -45,7 +49,6 @@ const getConsonant = () => {
       // setInputWords((prevWords) => [...prevWords, inputValue]);
       // setInputValue("");
     }
-  };
 
   const setConsonant = () => {
     axios({
@@ -78,6 +81,7 @@ const getConsonant = () => {
   const subscribeSocket = () => {
     client.current.connect({}, () => {
       client.current.subscribe(`/topic/game/${roomId}`, (response) => {
+        console.log('inputValues------------->', inputValue)
         var data = JSON.parse(response.body);
         console.log(data);
         console.log('틀렸or맞았', data.correct);
@@ -85,14 +89,16 @@ const getConsonant = () => {
         if (data.correct) {
           setInputWords((prevWords) => [...prevWords, inputValue])
           console.log('data.correct', data.correct)
-          console.log('inputWords', inputWords)
           console.log('inputValue', inputValue)
+          console.log('inputWords', inputWords)
+          console.log('currentword', currentword)
         }
         else{
           console.log('data.correct', data.correct)
           alert(data.msg)
         }
-        setInputValue("");
+        // setInputValue(""); 
+        dispatch(saveWord({input: ''}))
       })  // 채팅 구독
     })
   }
@@ -106,7 +112,7 @@ const getConsonant = () => {
     setConsonant();
     const timeout = setTimeout(() => {
       setShowModal(false);
-    }, 5000);  // 설명 모달 시간 설정! 7초 정도? 임시로 1초
+    }, 1000);  // 설명 모달 시간 설정! 7초 정도? 임시로 1초
 
     return () => {
       clearTimeout(timeout);
@@ -120,8 +126,8 @@ const getConsonant = () => {
         <div className={styles.modalContainer}>
           <div className={styles.modalContent}>
             <p>10초 안에 제시된 초성과 일치하는 단어를 입력하세요.</p>
-            <p>*세종대왕님이 보고 계십니다*</p>
-            <p>*사전에 등재된 단어만 입력해주세요.*</p>
+            <p>*두 글자의 단어만 입력 가능합니다.*</p>
+
             <h4>제시된 초성: {randomConsonant}</h4>
           </div>
         </div>
@@ -157,7 +163,7 @@ const getConsonant = () => {
         <br />
         <div className={styles.redBlock}>
           <img
-            src="/세종대왕_기본.png"
+            src="/초성_세종대왕_기본.png"
             style={{
               position: "absolute",
               left: "125px",
@@ -178,7 +184,7 @@ const getConsonant = () => {
             초성: {randomConsonant}
           </h3>
           <img
-            src="/두루마리.png"
+            src="/초성_두루마리.png"
             style={{
               position: "absolute",
               width: "700px",
