55d9452ffcc5e5f7e824a400ccd599a490b9a183
You Young <dbdud1999@naver.com>
2023-08-16 14:19:38 +0900

    Feat: #642[유영] STT 적용

--- frontend/pages/enter/index.js
+++ frontend/pages/enter/index.js
@@ -17,21 +17,7 @@ export default function EnterPage() {
   const router = useRouter();
   const dispatch = useDispatch();
 
-  /* 유영 : 소켓 간단 연결 작업 시작 */
-  useEffect(() => {
-    const socket = new SockJS("http://localhost:80/ws");
-    const stompClient = Stomp.over(socket);
-
-    stompClient.connect(
-      {},
-      /*Connect Callback*/ () => {
-        console.log("Socket Connected.");
-      }
-    );
-  }, []);
-  /* 유영 : 소켓 간단 연결 작업 끝 */
-
-  /* 유영 : axios를 통한 닉네임 생성 및 방 생성 시작 */
+  const audio = new Audio('/music/enter_bgm.mp3');
   /* 희진 : axios 렌더링 타이밍 변경 시작 (페이지 로딩 시 최초 1회) */
   let roomId = "";
   let progress = false;
@@ -65,6 +51,12 @@ export default function EnterPage() {
     getUserCamera();
   }, [videoRef]);
   /* 혜지 : 웹캠 화면 띄우기 위한 구현 끝 */
+
+  useEffect(() => {
+      playerBGM();
+      window.addEventListener("beforeunload", onbeforeunload);
+  }, []);
+  
 
   const [text, setText] = useState("");
 
@@ -139,45 +131,21 @@ export default function EnterPage() {
         }
       });
   };
-  /* 유영 : axios를 통한 닉네임 생성 및 방 생성 끝 */
   /* 희진 : axios 렌더링 타이밍 변경 끝 */
 
-  /* 희진 : 추후 삭제 예정 시작 */
-  // const tmp = (
-  //   <div className="first" >
+  const playerBGM = async() => {
+    /*
+      ✔ Music provided by 셀바이뮤직
+      🎵 Title : 배달은 자신있어 by 배달의민족
+      https://sellbuymusic.com/md/micwcfw-jcncnhn
+    */
+    await audio.play();
+  };
 
-  //     {/* <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}> */}
-  //     <div className="title">
-  //       {/* <img src="/tent.png" />
-  //       <br /> */}
-  //       <img src="/main/title.png" />
-  //       {/* <br /> */}
-  //     </div>
-
-  //     <div style={{ display: 'flex' }}>
-  //       <div className='leftBox'>
-  //         <div style={{ display: 'flex' }}>
-  //           <div className='imgCircle'></div>
-  //           <div className='inputNickname'>
-  //             <p style={{ fontSize: '20px' }}>캐릭터와 닉네임 선택</p>
-  //             <br />
-  //             <label htmlFor=""></label>
-  //             <input
-  //               value={text}
-  //               onChange={handleOnChange}
-  //               onKeyDown={enterDown}
-  //             />
-  //           </div>
-  //           <button id='startBtn' onClick={gameStart}>START</button>
-  //         </div>
-  //       </div>
-
-  //       <RightBox />
-  //       {/* <p id="bottom">서비스 약관 | 개인정보 취급정보 | 문의</p> */}
-  //     </div>
-  //   </div>
-  // )
-  /* 희진 : 추후 삭제 예정 끝 */
+  const onbeforeunload = (e) => {
+    audio.pause();
+    audio.currentTime = 0;
+  };
 
   return (
     <div className={styles.container}>
