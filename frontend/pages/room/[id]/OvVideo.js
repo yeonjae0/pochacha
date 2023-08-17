d2e93e2ffa31c1dac2d5d8b17e3eb863a48d921c
유영 <dbdud1999@naver.com>
2023-08-18 02:03:21 +0900

    Feat: #643[유영] 페이스필터 적용

--- frontend/pages/room/[id]/OvVideo.js
+++ frontend/pages/room/[id]/OvVideo.js
@@ -1,35 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useRef, useState } from "react";
 import styles from "@/styles/UserVideo.module.css";
+import * as deepar from "deepar";
 
-export default class OpenViduVideoComponent extends Component {
+// import * as canvas from 'canvas';
+// import * as faceapi from 'face-api.js';
 
-    constructor(props) {
-        super(props);
-        this.videoRef = React.createRef();
+//export default class OpenViduVideoComponent extends Component {
+
+export default function OpenViduVideoComponent(props) {
+  const videoRef = React.createRef();
+
+  /// 여기서부터 Deep AR 변수
+  const canvasRef = useRef(null); // Deep AR Canvas
+  let deepAR = null;
+
+  useEffect(() => {
+    if (props && !!videoRef) {
+      props.streamManager.addVideoElement(videoRef.current);
     }
+  });
 
-    componentDidUpdate(props) {
-        if (props && !!this.videoRef) {
-            this.props.streamManager.addVideoElement(this.videoRef.current);
-        }
+  /// 여기서부터 Deep AR
+  const initializeDeepAR = async () => {
+    try {
+      const canvas = canvasRef.current;
+      const video = videoRef.current;
+      if (!deepAR) {
+        await console.log("deepAR ::: ", deepAR);
+        await console.log("deepar ::: ", deepar);
+
+        deepAR = await deepar.initialize({
+          licenseKey:
+            "278cc7b9cd550b7b553b0c6b3629c269ea59e76ba24c8393a927b41264da4ff4ca7c1a0d1e640e90",
+          canvas,
+          effect: "/effects/ray-ban-wayfarer.deepar",
+          additionalOptions: {
+            cameraConfig: {
+              disableDefaultCamera: true,
+            },
+          },
+        });
+        await deepAR.setVideoElement(video, true);
+        await deepAR.startCamera();
+      }
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    async componentDidMount() {
+  useEffect(() => {
+    initializeDeepAR();
+    return () => {
+      if (deepAR) {
+        deepAR.shutdown();
+      }
+    };
+  }, [videoRef]);
 
-        if (this.props && !!this.videoRef) {
-            this.props.streamManager.addVideoElement(this.videoRef.current);
-        }
-
-
-    }
-
-    render() {
-        return (
-            <div>
-                <video autoPlay={true} ref={this.videoRef} className={styles.videofilter}/>
-                {/* <canvas/> */}
-            </div>
-        )
-    }
-
+  // render() {
+  return (
+    <div>
+      <canvas ref={canvasRef}>
+        <video autoPlay={true} ref={videoRef} className={styles.videofilter} />
+      </canvas>
+    </div>
+  );
+  // }
 }
